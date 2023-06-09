73b3a554413f678b36f32811da9070c5b5a53e06
likhithreddy28 <likhithreddy150@gmail.com>
2023-06-09 14:43:23 +0530

    Tablejs

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 const Table = () => {
-    const numbers = [1 ,2 ,3 ,4 ,5 ,6, 7 ,8 ,9 ,10 , 11 ,12 ]
+    const numbers = [1 ,2 ,3 ,4 ,5 ,6, 7 ,8 ,9 ,10 , 11 ,12, 13 , 14 , 15 , 16]
+    const inActiveBtn = "btn btn-outline-primary m-2"
+    const ActiveBtn = "btn btn-outline-primary m-2 active"
+
+    const [ number , SetNumber] = React.useState(null)
   return (
     <div>
         <center>
            {numbers.map((num ,index)=>{
             <div style={{display : 'inline'}} key={index}> 
-                <button  className=' btn btn-outline-primary'>
+                <button  className = {number ===num? ActiveBtn : inActiveBtn} onClick={()=> SetNumber(num) }>
                       {num}
                 </button>
             </div>
