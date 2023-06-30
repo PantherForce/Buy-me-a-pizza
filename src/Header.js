29d6c8f257a20e85afc60223cbc1796a0562a9e5
likhithreddy28 <likhithreddy150@gmail.com>
2023-06-30 15:54:54 +0530

    changess

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,48 @@
+import { AppBar, Container, Link, Typography, makeStyles  } from '@material-ui/core'
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-
+const usestyles = makeStyles (()=>({
+       nav : {
+        display : 'flex',
+        justifyContent : "center" ,
+        alignItems : "center",
+        flexDirection : 'row' , 
+       }
+}))
 const Header = () => {
+
+  const  classes = makeStyles () ;
+
   return (
-    <div>
-        <nav className='navbar navbar-light bg-light'>
-            <a className='navbar-brand'>Resturant</a>
-            <button className='btn btn-primary'>
-                orders <span className='badge badge-light'></span>
-            </button>
-        </nav>
-    </div>
+  <AppBar variant='static'>
+    <Container>
+      <Typography variant='h3'>
+        Pizza Man 
+      </Typography>
+
+      <div className ={classes.nav}>
+       <NavLink>
+        
+        <Link to ='./'>
+          Home
+        </Link>
+
+        <Link to ='./'>
+          Order
+        </Link>
+        <Link to ='./'>
+          Table
+        </Link>
+        <Link to ='./'>
+          Contact us
+        </Link>
+
+       </NavLink>
+      </div>
+
+    </Container>
+  </AppBar>
   )
 }
 
