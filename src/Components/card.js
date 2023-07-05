61c5b0bbe901d0c74a109ecad2fcac9dfa459919
likhithreddy28 <likhithreddy150@gmail.com>
2023-07-05 22:18:13 +0530

    card.js

---
+++ src/Components/card.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+const usestyles = makeStyles(()=>({
+      card : {
+            
+      }
+}))
+
+const card = () => {
+
+    const classes = usestyles();
+  return (
+    <div>card</div>
+  )
+}
+
+export default card
